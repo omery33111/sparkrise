a9652147c11e5f6defbff3caa90adb7c2a0e937a
Omer Yanai <omery33111@gmail.com>
2023-07-19 02:13:35 +0300

    allowed origins

--- front-end/src/endpoints/endpoints.ts
+++ front-end/src/endpoints/endpoints.ts
@@ -2,34 +2,34 @@
 
 
 // ------------------------- SCHEDULER ENDPOINTS:
-export const schedulerURL = "http://127.0.0.1:8000/scheduler";
+// export const schedulerURL = "http://127.0.0.1:8000/scheduler";
 
-// export const schedulerURL = "https://business-sparkrise.onrender.com/scheduler";
+export const schedulerURL = "https://business-sparkrise.onrender.com/scheduler";
 
 
 
 // ------------------------- CALLBACK ENDPOINTS:
-export const callbackURL = "http://127.0.0.1:8000/callback";
+// export const callbackURL = "http://127.0.0.1:8000/callback";
 
-// export const callbackURL = "https://business-sparkrise.onrender.com/callback";
-
-
-
-// ------------------------- AUTHENTICATION ENDPOINTS:
-export const loginURL = "http://127.0.0.1:8000/authentication/login/";
-
-// export const loginURL = "https://business-sparkrise.onrender.com/authentication/login/";
+export const callbackURL = "https://business-sparkrise.onrender.com/callback";
 
 
 
 // ------------------------- AUTHENTICATION ENDPOINTS:
-export const appointmentURL = "http://127.0.0.1:8000/appointment";
+// export const loginURL = "http://127.0.0.1:8000/authentication/login/";
 
-// export const appointmentURL = "https://business-sparkrise.onrender.com/appointment";
+export const loginURL = "https://business-sparkrise.onrender.com/authentication/login/";
+
+
+
+// ------------------------- AUTHENTICATION ENDPOINTS:
+// export const appointmentURL = "http://127.0.0.1:8000/appointment";
+
+export const appointmentURL = "https://business-sparkrise.onrender.com/appointment";
 
 
 
 // ------------------------- ADMINISTRATOR ENDPOINTS:
-export const administratorURL = "http://127.0.0.1:8000/administrator";
+// export const administratorURL = "http://127.0.0.1:8000/administrator";
 
-// export const administratorURL = "https://business-sparkrise.onrender.com/administrator";
+export const administratorURL = "https://business-sparkrise.onrender.com/administrator";
