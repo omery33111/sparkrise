bba613eb5c462e0a44ad10f4b4d6b06c62dd49f9
Omer Yanai <omery33111@gmail.com>
2023-07-19 01:50:52 +0300

    render endpoints

--- front-end/src/endpoints/endpoints.ts
+++ front-end/src/endpoints/endpoints.ts
@@ -1,32 +1,35 @@
 
 
 
-// ------------------------- SERVER ENDPOINT:
-// export const myServer = "http://127.0.0.1:8000";
-
-// export const myServer = "https://ecommerce-lops.onrender.com";
-
-
-
 // ------------------------- SCHEDULER ENDPOINTS:
-export const schedulerURL = "http://127.0.0.1:8000/scheduler";
+// export const schedulerURL = "http://127.0.0.1:8000/scheduler";
+
+export const schedulerURL = "https://business-sparkrise.onrender.com/scheduler";
 
 
 
 // ------------------------- CALLBACK ENDPOINTS:
-export const callbackURL = "http://127.0.0.1:8000/callback";
+// export const callbackURL = "http://127.0.0.1:8000/callback";
+
+export const callbackURL = "https://business-sparkrise.onrender.com/callback";
 
 
 
 // ------------------------- AUTHENTICATION ENDPOINTS:
-export const loginURL = "http://127.0.0.1:8000/authentication/login/";
+// export const loginURL = "http://127.0.0.1:8000/authentication/login/";
+
+export const loginURL = "https://business-sparkrise.onrender.com/authentication/login/";
 
 
 
 // ------------------------- AUTHENTICATION ENDPOINTS:
-export const appointmentURL = "http://127.0.0.1:8000/appointment";
+// export const appointmentURL = "http://127.0.0.1:8000/appointment";
+
+export const appointmentURL = "https://business-sparkrise.onrender.com/appointment";
 
 
 
 // ------------------------- ADMINISTRATOR ENDPOINTS:
-export const administratorURL = "http://127.0.0.1:8000/administrator";
+// export const administratorURL = "http://127.0.0.1:8000/administrator";
+
+export const administratorURL = "https://business-sparkrise.onrender.com/administrator";
