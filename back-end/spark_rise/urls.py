edfa50fa9d82225c81fa7e76da184215f8bea98c
Omer Yanai <omery33111@gmail.com>
2023-07-19 18:32:46 +0300

    fixed

--- back-end/spark_rise/urls.py
+++ back-end/spark_rise/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +14,9 @@ urlpatterns = [
     path('appointment/', include('appointment.urls')),
     path('administrator/', include('administrator.urls')),
 ]
+
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
